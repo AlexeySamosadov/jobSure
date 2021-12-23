f8accc76e793fea5c5b04c96cefe2e8bae576fa3
Самосадов Алексей Вадимович <asamosadov@phoenixit.ru>
2021-12-24 00:03:57 +0300

    add article and article fixes

--- routes/article.js
+++ routes/article.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res)=> {
 //       } catch (e) {
 //          console.log(e)
 //       }
-
+   console.log('article', article)
 
     res.render('article', {
        title: article.title,
